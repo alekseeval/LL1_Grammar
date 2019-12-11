1189d8322aac4af07d4fe1433d41af0e057a981e
HyperMarshmallow <alekseeval2000@mail.ru>
2019-12-11 16:37:33 +0800

    Initial commit

---
+++ src/com/company/DownParser.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class DownParser {
+
+    // Отображение терминалов в цифры для удобства использования таблицы
+    HashMap<Character, Integer> terminals;
+    // Отображение НЕ терминалов в цифры
+    HashMap<Character, Integer> nonTerminals;
+    // Массив правил вида [["S", "Pe"], ["P", "QR"] ...]
+    // т.е. он всегда двумерный
+    String[][] rules;
+    // Таблица разбора, внутри которой номера применяемых правил
+    int[][] table;
+
+    public DownParser(HashMap<Character, Integer> terminals, HashMap<Character, Integer> nonTerminals, String[][] rules, int[][] table) {
+        this.terminals = terminals;
+        this.nonTerminals = nonTerminals;
+        this.rules = rules;
+        this.table = table;
+    }
+
+    String getDef(String word){
+
+        // Использовать стек? Тогда как строить вывод (т.е. на каждой иттерации добавлять переход)?
+        // Или юзать StringBuffer?
+        // Делать иттерацию внутри цикла while(!word.equals("e")) , где 'e' - конец строки? (для стека просто поднимать верхний элемент)
+
+        // Стек для хранения вывода
+        Stack<Character> parse = new Stack<>();
+        // Стек для хранения строки
+        Stack<Character> str = new Stack<>();
+
+        // Заполнение стека со строкой
+        for (int i = word.length() - 1; i >= 0; i--) {
+            str.add(word.charAt(i));
+        }
+        parse.add('F'); // Начальный символ - S
+
+        // Строка вывода
+        String  current = "F";
+        StringBuilder output = new StringBuilder("F");
+
+        int count = 0; // Позиция в строке, на которой мы стоим (текущий символ)
+        while (!str.empty()){
+            current = current.substring(1); // Убрать из current первый символ
+
+
+            // Получаем из стеков значения сверху
+            char nonTerm = parse.pop();
+            char term = str.pop();
+
+            // Если в стеке нетерминалов лежит терминал, то убираем его из двух стеков
+            if (nonTerm < 'A' || nonTerm > 'Z'){
+                count++;
+                continue;
+            }
+            output.append(" -> ");
+
+            str.add(term); // Возвращаем терминал обратно в стек
+            // Получаем номер правила из таблица разбора
+            int ruleNumber = this.table[this.nonTerminals.get(nonTerm)][this.terminals.get(term)];
+
+            // Получаем правую часть правила
+            String right = this.rules[ruleNumber][1];
+
+            if (right.equals("E")){
+                output.append(word.substring(0, count))
+                        .append(current);
+                continue;
+            }
+
+            // Заменить current строку
+            current = right + current;
+            // Добавить current в output
+            output.append(word.substring(0, count))
+                    .append(current);
+
+            // Кладем правую часть правила в стек
+            for (int i = right.length()-1; i >= 0 ; i--) {
+                parse.add(right.charAt(i));
+            }
+
+        }
+
+        return output.toString();
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // Считывание из файла соответствующих данных
+        HashMap<Character, Integer>[] t = readTerminalsFromFile("terms1.txt");
+        String[][] rules = readRulesFromFile("rules1.txt");
+        int[][] table = readTableFromFile("table1.txt");
+
+        // Создание экземпляра класса, который будет строить вывод
+        DownParser dp = new DownParser(t[0], t[1], rules, table);
+
+        // Считывание слова из консоли
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your word >> ");
+//        String word = in.next();
+//        String word = "(n+x*n)*(n-n)e"
+        String word = "(n)e";
+
+        System.out.println("Построенный вывод:");
+        System.out.println(dp.getDef(word));
+
+    }
+
+    // Считывание таблицы разбора
+    private static int[][] readTableFromFile(String str) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File(str));
+        String[] size = in.nextLine().split(" ");
+        int row = Integer.parseInt(size[0]);
+        int col = Integer.parseInt(size[1]);
+        int[][] res = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                res[i][j] = in.nextInt();
+            }
+        }
+
+        return res;
+    }
+
+    // Считывание терминалов [0] и нетерминалов [1]
+    private static HashMap<Character, Integer>[] readTerminalsFromFile(String str) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File(str));
+        HashMap<Character, Integer>[] res = new HashMap[2];
+
+        for (int i = 0; i < 2; i++) {
+            res[i] = new HashMap<>();
+            String[] line = in.nextLine().split(" ");
+            for (int j = 0; j < line.length; j++) {
+                res[i].put(line[j].charAt(0), j);
+            }
+        }
+
+        return res;
+    }
+
+    // Считывание правил
+    private static String[][] readRulesFromFile(String str) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File(str));
+        int size = Integer.parseInt(in.nextLine());
+        String[][] res = new String[size][];
+
+        for (int i = 0; in.hasNext(); i++) {
+            res[i] = in.nextLine().split(" ", 2);
+        }
+
+        return res;
+    }
+}
